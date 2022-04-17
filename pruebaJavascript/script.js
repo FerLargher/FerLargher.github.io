069712e32c6a1af02d3fd4def27e9b685ef250d4
Largher <fernando.largher@gmail.com>
2022-04-17 15:19:49 -0300

    terminando proyecto 1

---
+++ pruebaJavascript/script.js
@@ -0,0 +1,9 @@
+/*let numA = parseInt(prompt("Dame un numero: "));
+let numB = parseInt(prompt("Dame otro numero: "));
+
+if (numA > numB) {
+    alert("El numero "+numA+" es el mayor.");
+} else {
+    alert("El numero "+numB+" es el mayor.");
+}*/
+
