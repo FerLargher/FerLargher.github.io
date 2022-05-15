9d0c2148ff3b4073a46761d19c2622476ab24fa5
Largher <fernando.largher@gmail.com>
2022-05-15 13:05:14 -0300

    eliminados archivos y carpetas innecesarias

--- pruebaJavascript/script.js
+++
@@ -1,9 +0,0 @@
-/*let numA = parseInt(prompt("Dame un numero: "));
-let numB = parseInt(prompt("Dame otro numero: "));
-
-if (numA > numB) {
-    alert("El numero "+numA+" es el mayor.");
-} else {
-    alert("El numero "+numB+" es el mayor.");
-}*/
-
